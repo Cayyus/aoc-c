47a283a03f7304c070a1d3d7c29a902376dafe0d
Caius <117181766+Cayyus@users.noreply.github.com>
2025-08-24 13:11:30 +0600

    part 2 day 3

--- 2016/day3.c
+++ 2016/day3.c
@@ -19,6 +19,36 @@ void part1(FILE *f) {
     printf("Valid triangles: %d\n", valid);
 }
 
+void part2(FILE *f) {
+    char line[100];
+    int i, j, k = 0;
+    int count = 0;
+    int valid = 0;
+    static int arr[1599][3] = {};
+
+    while (fgets(line, sizeof(line), f)) {
+        if (sscanf(line, "%d %d %d", &i, &j, &k) == 3) {
+            arr[count][0] = i;
+            arr[count][1] = j;
+            arr[count][2] = k;
+            count++;
+        }
+    }
+
+    for (int row = 0; row < count; row += 3) {
+        for (int col = 0; col < 3; col++) {
+            int a = arr[row][col];
+            int b = arr[row + 1][col];
+            int c = arr[row + 2][col];
+
+            if (a + b > c && b + c > a && c + a > b) {
+                valid++;
+            }
+        }
+    }
+    printf("Valid vertical triangles: %d\n", valid);
+}
+
 int main() {
     FILE *f = fopen("aoc.txt", "r");
     if (!f) {
@@ -26,6 +56,7 @@ int main() {
         return 1;
     }
     part1(f);
+    part2(f);
     fclose(f);
     return 0;
 }
